939240efbd9572418f268b5dfa506862133850c7
maqueredkop <maqueyz@163.com>
2017-11-29 09:05:48 +0800

    update the demo

--- unordered_list.py
+++ unordered_list.py
@@ -40,5 +40,54 @@ class Unordered_list:
         temp = Node(item)
         temp.set_next(self.head)
         self.head = temp
+ 
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.get_next()
+            
+        return count
+    
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
         
+        return found
+    
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+                
+        if previous == None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+            
+            
+my_unorder_list = Unordered_list()
+my_unorder_list.is_empty()
+my_unorder_list.size()
+my_unorder_list.add(21)
+my_unorder_list.add(56)
+my_unorder_list.add(78)
+my_unorder_list.is_empty()
+my_unorder_list.size()
+my_unorder_list.search(56)
+my_unorder_list.search(100)
+my_unorder_list.remove(21)
+my_unorder_list.size()
     
